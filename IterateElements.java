eddc6f6345180064c2b8c4613d83f3f07d0406bb
Krutika9021 <109949451+Krutika9021@users.noreply.github.com>
2022-07-30 11:59:50 -0800

    Add files via upload

---
+++ IterateElements.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class IterateElements               
+{
+  public static void main(String args[])
+  {
+      ArrayList<String> languages = new ArrayList<>();
+      
+      languages.add("Java");
+      languages.add("Python");
+      languages.add("c++");
+      System.out.println("ArrayList: ");
+      languages.forEach((e) -> 
+      {
+          System.out.println(e+", ");
+      });
+  }
+}
