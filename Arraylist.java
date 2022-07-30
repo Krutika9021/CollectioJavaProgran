da3445a6b23d1b323ce9057389f43f661edaa638
Krutika9021 <109949451+Krutika9021@users.noreply.github.com>
2022-07-30 11:52:09 -0800

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,15 @@
+package arraylist;
+import java.util.ArrayList;
+
+class Main 
+{
+  public static void main(String[] args)
+  {
+    ArrayList<String> languages = new ArrayList<>();
+
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("c++");
+    System.out.println("ArrayList: " + languages);
+  }
+}
